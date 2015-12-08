ecc53573bd968ed5c2d5d98179c600fa4f8af4f9
Gethe <gethe88@gmail.com>
2015-12-07 20:12:13 -0600

    Clean up project files

--- apply_patches.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-Dir["/Users/tekkub/Downloads/*.diff"].each do |f|
-  /\d\.\d\.\d\.(?<before>\d+)-(?<after>\d+)\.diff$/ =~ f
-  puts "\n~~~~~~~~~~~~~~ Applying build #{after} ~~~~~~~~~~~~~~"
-  puts `git apply "#{f}"`
-  puts `git add .`
-  puts `git commit -m 'Build #{after}'`
-end
