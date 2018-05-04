77815bba20d72b5e924821bcec875c725850d5cb
Tim West <gethe88@gmail.com>
2018-05-04 13:33:57 -0500

    8.0.1 (26557)

--- SharedXML/SharedBasicControls.lua
+++ SharedXML/SharedBasicControls.lua
@@ -44,6 +44,7 @@ function ScriptErrorsFrameMixin:OnShow()
 end
 
 function ScriptErrorsFrameMixin:DisplayMessageInternal(msg, warnType, keepHidden, locals, msgKey)
+	addframetext("Lua Error: "..msgKey);
 	local index = self.seen[msgKey];
 	if ( index ) then
 		self.count[index] = self.count[index] + 1;
